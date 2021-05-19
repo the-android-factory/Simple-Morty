c5aea0693e5c6bd13afbc2e8c082bdd79e8d6bb9
developer-dmp <developer.dmp@gmail.com>
2021-05-18 23:03:24 -0400

    Prevented clipping and renamed layout file.

--- app/src/main/java/com/dmp/simplemorty/CharacterDetailActivity.kt
+++ app/src/main/java/com/dmp/simplemorty/CharacterDetailActivity.kt
@@ -17,7 +17,7 @@ class CharacterDetailActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_character_details)
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
