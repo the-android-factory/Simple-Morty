1c44b9b4a68938a4ed168a2a16221beb48515f2d
developer-dmp <developer.dmp@gmail.com>
2021-06-10 07:57:03 -0400

    Added DrawerLayout to the app and connected it with the NavGraph.

---
+++ app/src/main/java/com/dmp/simplemorty/EpisodeListFragment.kt
@@ -0,0 +1,26 @@
+package com.dmp.simplemorty
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.dmp.simplemorty.databinding.FragmentEpisodeListBinding
+
+class EpisodeListFragment: Fragment(R.layout.fragment_episode_list) {
+
+    private var _binding: FragmentEpisodeListBinding? = null
+    private val binding: FragmentEpisodeListBinding by lazy {
+        _binding!!
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentEpisodeListBinding.bind(view)
+
+        // todo
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
--- app/src/main/java/com/dmp/simplemorty/NavGraphActivity.kt
+++ app/src/main/java/com/dmp/simplemorty/NavGraphActivity.kt
@@ -2,11 +2,14 @@ package com.dmp.simplemorty
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.Navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
 
 class NavGraphActivity : AppCompatActivity() {
 
@@ -19,12 +22,18 @@ class NavGraphActivity : AppCompatActivity() {
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
+        val drawerLayout = findViewById<DrawerLayout>(R.id.drawer_layout)
 
-        appBarConfiguration = AppBarConfiguration(navController.graph)
+        appBarConfiguration = AppBarConfiguration(
+            topLevelDestinationIds = setOf(R.id.characterListFragment, R.id.episodeListFragment),
+            drawerLayout = drawerLayout
+        )
         setupActionBarWithNavController(
             navController = navController,
             configuration = appBarConfiguration
         )
+
+        findViewById<NavigationView>(R.id.nav_view).setupWithNavController(navController)
     }
 
     override fun onSupportNavigateUp(): Boolean {
